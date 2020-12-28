4357044adf33b26e58788e495c14e07f6bfb4a41
Mandera <rickard.abraham@gmail.com>
2020-12-28 12:20:11 +0100

    Working but it added .idea.

--- generalpackager/api/local_repo.py
+++ generalpackager/api/local_repo.py
@@ -72,7 +72,7 @@ class LocalRepo:
 
         # print(repo.index.)
 
-        repo.git.add(all=True)
+        repo.git.add(u=True)
         repo.index.commit("Working but it added .idea.")
         remote = repo.remote()
         remote.set_url(f"https://Mandera:{os.environ['packager_github_api']}@github.com/ManderaGeneral/generalpackager.git")
