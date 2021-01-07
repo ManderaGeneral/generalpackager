f5438a0d2b7bf229e672b2dfb0f02c3de89a8a8d
Mandera <rickard.abraham@gmail.com>
2021-01-07 14:04:37 +0100

    Removed workflow if.

--- randomtesting.py
+++ randomtesting.py
@@ -10,9 +10,6 @@ packager = Packager("generalpackager")
 # packager.generate_workflow()
 packager.setup_all("Trying to make github token work.")
 
-# HERE ** Next up is workflow generation
-
-# packager.generate_workflow()
 
 # Todo: Automatically insert empty line when indent retracts
 
