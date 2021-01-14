7dfbbe21d2e4af7b1b973494f7834f2cc3083a3d
runner <runner@fv-az16-964>
2021-01-14 14:06:45 +0000

    [CI SYNC]

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name="generalpackager",
     author='Rickard "Mandera" Abraham',
     author_email="rickard.abraham@gmail.com",
-    version="0.0.5",
+    version="0.0.6",
     description="Tools to interface GitHub, PyPI and local modules / repos. Used for generating files to keep projects dry and synced.",
     long_description=Path(r'README.md').read_text(encoding='utf-8'),
     long_description_content_type="text/markdown",
@@ -29,7 +29,6 @@ setup(
         'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
         'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS',
         'Operating System :: POSIX :: Linux',
     ],
 )
