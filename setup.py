15a2329a2c074184b94c2441b94651c5481cb292
runner <runner@fv-az18-873>
2021-01-15 11:48:26 +0000

    [CI SYNC]

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     extras_require={},
     classifiers=[
         'Topic :: Software Development :: Build Tools',
-        'Topic :: Software Development :: Libraries',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
