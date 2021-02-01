33bbb7b18ad560ca2d7755fd20d19924f363048e
Mandera <rickard.abraham@gmail.com>
2021-02-01 11:34:59 +0100

    [CI SKIP] Testing

--- generalpackager/api/local_repo.py
+++ generalpackager/api/local_repo.py
@@ -155,7 +155,7 @@ class LocalRepo:
 
         push = remote.push()
         pushInfo = push[0]
-        print(push, dir(pushInfo), pushInfo.local_ref, pushInfo.remote_ref)
+        print(push, dir(pushInfo), pushInfo.local_ref, pushInfo.remote_ref, pushInfo.summary)
 
     def get_changed_files(self):
         """ Get a list of changed files compared to remote. """
