e449c4a08063542bc236f480c3f16fc33812f1d1
Mandera <rickard.abraham@gmail.com>
2021-02-09 09:17:30 +0100

    Added from_builtin() to filter for attrs.

--- generalpackager/api/local_module.py
+++ generalpackager/api/local_module.py
@@ -29,7 +29,7 @@ class LocalModule:
     def _filter(self, objInfo):
         """ :param ObjInfo objInfo: """
         is_part_of_module = objInfo.module().__name__.startswith(self.module.__name__)
-        return objInfo.public() and not (objInfo.is_instance() or objInfo.is_module()) and is_part_of_module and objInfo.name not in ("fget", "fset")
+        return objInfo.public() and not (objInfo.is_instance() or objInfo.is_module()) and is_part_of_module and objInfo.name not in ("fget", "fset") and not objInfo.from_builtin()
 
     def get_env_vars(self):
         """ Get a list of EnvVar instances avialable directly in module.
