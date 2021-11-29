156f9ec195dbdf2bf6749242ba8a941d188eb933
runner <runner@fv-az203-964>
2021-11-29 18:00:38 +0000

    [CI AUTO] Sync triggered by manderageneral

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="generalpackager",
     author='Rickard "Mandera" Abraham',
     author_email="rickard.abraham@gmail.com",
-    version="0.3.10",
+    version="0.3.11",
     description="Tools to interface GitHub, PyPI and local modules / repos. Used for generating files to keep projects dry and synced. Tailored for my general packages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
