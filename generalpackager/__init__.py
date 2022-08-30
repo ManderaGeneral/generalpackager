cd9de297f0f8b4b46011dfb9e05ee624bb0558c1
Mandera <rickard.abraham@gmail.com>
2022-08-30 10:09:14 +0800

    Moved interconnect to lib

--- generalpackager/__init__.py
+++ generalpackager/__init__.py
@@ -1,23 +1,12 @@
+from generallibrary import interconnect
 
-from generalpackager.other.envvars import PACKAGER_GITHUB_API, TWINE_USERNAME, TWINE_PASSWORD
-
-from generalpackager.packager import Packager
-
-from generalpackager.api.localrepo.base.localrepo import LocalRepo
-from generalpackager.api.localrepo.python.localrepo_python import LocalRepo_Python
-from generalpackager.api.localrepo.node.localrepo_node import LocalRepo_Node
-
-from generalpackager.api.localmodule import LocalModule
 from generalpackager.api.github import GitHub
+from generalpackager.api.localmodule import LocalModule
+from generalpackager.api.localrepo.base.localrepo import LocalRepo
+from generalpackager.api.localrepo.node.localrepo_node import LocalRepo_Node
+from generalpackager.api.localrepo.python.localrepo_python import LocalRepo_Python
 from generalpackager.api.pypi import PyPI
-
-
-def interconnect(*objs):
-    """ Todo: Put interconnect in lib and test. """
-    for obj in objs:
-        for obj2 in objs:
-            if obj is obj2:
-                continue
-            setattr(obj, obj2.__name__, obj2)
+from generalpackager.other.envvars import PACKAGER_GITHUB_API, TWINE_USERNAME, TWINE_PASSWORD
+from generalpackager.packager import Packager
 
 interconnect(Packager, LocalRepo, LocalModule, GitHub, PyPI)
