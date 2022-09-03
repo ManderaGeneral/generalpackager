5b81bc0b9451fd7da1b3e382e2847c8dec8a00a1
Mandera <rickard.abraham@gmail.com>
2022-09-03 17:57:37 +0800

    Removing tests

--- generalpackager/test/tests/repo/generalpackager-0.5/generalpackager/api/localrepo/node/metadata_node.py
+++
@@ -1,5 +0,0 @@
-from generalpackager.api.localrepo.base.metadata import Metadata
-
-class Metadata_Node(Metadata):
-    dependencies = []
-    devDependencies = ["jest-environment-jsdom", "parcel"]
--- generalpackager/test/tests/repo/generalpackager-0.5/generalpackager/test/test_local_repo_node.py
+++
@@ -1,6 +0,0 @@
-from generalfile.test.test_path import PathTest
-
-
-class TestLocalRepo_Node(PathTest):
-    pass
-
