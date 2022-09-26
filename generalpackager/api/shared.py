210da95e5a068d2c912fed35c6ecafe668d3c42f
Mandera <rickard.abraham@gmail.com>
2022-09-26 20:04:21 +1000

    Allowing . in name cus of .github

--- generalpackager/api/shared.py
+++ generalpackager/api/shared.py
@@ -42,7 +42,7 @@ class _SharedName:
     @deco_cache()
     def _trim_partial(cls, name):
         if name:
-            return re.match("(\w|-)+", name).group()
+            return re.match("(\w|[-.])+", name).group()
 
     @classmethod
     @deco_cache()
