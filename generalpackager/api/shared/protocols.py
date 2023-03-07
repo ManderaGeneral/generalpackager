f0b9311dec8214de53d0893ad329e3c7602eb952
Mandera <rickard.abraham@gmail.com>
2023-03-07 13:49:38 +0800

    Metaclass limit phrasing

--- generalpackager/api/shared/protocols.py
+++ generalpackager/api/shared/protocols.py
@@ -1,6 +1,6 @@
 """ Not using typing.Protocol because that's for type checking only.
-    Not using abc because I don't want to define an extra metaclass.
-        The only thing that'd be nice is if it'd detect unchanged stubs. """
+    Not using abc because I can't define another metaclass.
+        The only thing that'd be nice is if it'd detect unchanged stubs without calling them. """
 from typing import List
 
 
