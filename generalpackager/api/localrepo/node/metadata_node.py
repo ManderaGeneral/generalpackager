dc202f6c11967bbe97ea718aec47a978c879c5be
Mandera <rickard.abraham@gmail.com>
2023-03-10 12:06:32 +0800

    Added missing target for Metadata_Node

--- generalpackager/api/localrepo/node/metadata_node.py
+++ generalpackager/api/localrepo/node/metadata_node.py
@@ -1,5 +1,6 @@
 from generalpackager.api.localrepo.base.metadata import Metadata
 
 class Metadata_Node(Metadata):
+    target = "node"
     dependencies = []
     devDependencies = ["jest-environment-jsdom", "parcel"]
