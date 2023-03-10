0300edc91bf8063923f6f15b92a823df876e7114
Mandera <rickard.abraham@gmail.com>
2023-03-10 14:41:53 +0800

    Made `_terminal` for LocalRepo_Node #12
    This solved being able to run `jest` because it required `.cmd` suffix.
    
    Need https://github.com/ManderaGeneral/generalpackager/issues/67 to setup test environment on actions

--- generalpackager/api/localrepo/node/localrepo_node.py
+++ generalpackager/api/localrepo/node/localrepo_node.py
@@ -1,7 +1,7 @@
 from typing import List
 
 from generalfile import Path
-from generallibrary import Terminal, Log, VerInfo, deco_require
+from generallibrary import Terminal, Log, deco_require
 from generalpackager.api.localrepo.base.localrepo import LocalRepo
 from generalpackager.api.localrepo.node.metadata_node import Metadata_Node
 
@@ -10,8 +10,16 @@ class LocalRepo_Node(LocalRepo):
     _cls_target = LocalRepo.Targets.node
     _cls_metadata = Metadata_Node
 
-    NPM_cmd = "npm.cmd" if VerInfo().windows else "npm"
     NODE_MODULES = "node_modules"
+    GLOBAL = "-g"
+
+    @classmethod
+    def _terminal(cls, cli, *args, local=None, **kwargs):
+        if local:
+            args = [s for s in args if s != cls.GLOBAL]
+
+        Log(__name__).debug(args)
+        return Terminal(cli, *args, **kwargs)
 
     @classmethod
     def _filt(cls, path):
@@ -24,12 +32,13 @@ class LocalRepo_Node(LocalRepo):
     def package_folder(self, local=True):
         if local:
             return self._node_modules_parent_path() / self.NODE_MODULES
-        npm_root = Terminal(self.NPM_cmd, "root", "-g").string_result
+
+        npm_root = self._terminal("npm", "root", "-g").string_result
         return Path(path=npm_root)
 
     def run_tests(self):
         with self.path.as_working_dir():
-            Terminal("jest", capture_output=False)
+            self._terminal("jest", capture_output=False)
 
     INSTALL = "install"
     UNINSTALL = "uninstall"
@@ -43,16 +52,9 @@ class LocalRepo_Node(LocalRepo):
         else:
             name = self.path.absolute()
 
-        args = [self.NPM_cmd, cmd, G := "-g", name]
-
-        if local:
-            args.remove(G)
-
-        Log(__name__).debug(args)
-
         package_folder = self.package_folder(local=local)
         with package_folder.as_working_dir():
-            Terminal(*args, capture_output=False)
+            self._terminal("npm", cmd, self.GLOBAL, name, capture_output=False)
 
     @deco_require(LocalRepo.exists)
     def install(self, local=True, editable=True):
@@ -64,19 +66,13 @@ class LocalRepo_Node(LocalRepo):
 
     @deco_require(LocalRepo.exists)
     def publish(self, public=True):
-        Log(__name__).debug(f"Publish for {self.name}")
-
         with self.path.as_working_dir():
-            Terminal(self.NPM_cmd, "publish", capture_output=False)
+            self._terminal("npm", "publish", capture_output=False)
 
     def _list_packages_gen(self, local=True, editable=None) -> List[str]:
-        args = [self.NPM_cmd, "list", "-g"]
-        if local:
-            args.remove("-g")
-
         package_folder = self.package_folder(local=local)
         with package_folder.as_working_dir():
-            terminal = Terminal(*args, error=False)
+            terminal = self._terminal("npm", "list", "-g", error=False)
 
         if terminal.fail:
             return
