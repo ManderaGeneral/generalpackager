76dbf04371dfd17b689cd59f182ff05148a352ff
Mandera <rickard.abraham@gmail.com>
2023-03-15 16:08:57 +0700

    Fixed return annotation for localrepo #68

--- generalpackager/api/localrepo/top/target_helper.py
+++ generalpackager/api/localrepo/top/target_helper.py
@@ -1,9 +1,11 @@
+from typing import Union
+
 from generalpackager.api.localrepo.base.localrepo import LocalRepo
 from generalpackager.api.localrepo.node.localrepo_node import LocalRepo_Node
 from generalpackager.api.localrepo.python.localrepo_python import LocalRepo_Python
 
 
-def localrepo(name=None, path=None, target=None) -> LocalRepo_Python | LocalRepo_Node:
+def localrepo(name=None, path=None, target=None) -> Union[LocalRepo_Python, LocalRepo_Node]:
     repo = LocalRepo(name=name, path=path)
     if target is None:
         target = repo.metadata.target
