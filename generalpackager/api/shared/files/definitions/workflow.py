c1182b3e7d04e211bd359d86516be7b43dc31f07
Mandera <rickard.abraham@gmail.com>
2023-04-22 07:31:06 +0700

    Trying latest pip #77

--- generalpackager/api/shared/files/definitions/workflow.py
+++ generalpackager/api/shared/files/definitions/workflow.py
@@ -151,7 +151,6 @@ class WorkflowFile(File):
 
     def _step_run_packager_method(self, method):
         run = CodeLine(f'run: |')
-        run.add_node(f'cd ..')
         run.add_node(f'python -c "from generalpackager import Packager; Packager().{method}()"')
         return self._get_step(f"Run Packager method '{method}'", run, self._get_env())
 
